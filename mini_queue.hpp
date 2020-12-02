1ca59af9cd311295fc1953a27e2ec7d1cd66233c
Eric Petersen <ericpetersen3.14@gmail.com>
2020-12-02 16:02:26 +0000

    Added features
    Added functions for viewing object last inserted, as well as the total size of the array through a constant expression. Also aligned the class to the byte boundary of the objects that it contains.

--- mini_queue.hpp
+++ mini_queue.hpp
@@ -5,14 +5,21 @@
 //No license, use at your own risk
 //Programmer: Eric Petersen
 template <class T, size_t S> //T is the variable to store, S is the max size of your queue
-class mini_queue {
-	T A[S];//Array to hold T variables, with size S
+class alignas(alignof(T)) mini_queue {
 	size_t F, R;//Front and Rear variables
+	T A[S];//Array to hold T variables, with size S
 public:
-	constexpr mini_queue() : F(0), R(0), A() {}//Initialize to zero
-	inline bool push(const T& V) { return size() <= S ? &(A[(R %= S)++] = V) : 0; }//Adds to queue, returns true on success
+		//Constructor
+	constexpr mini_queue() : F(0), R(0), A() {}//initialize all to zero
+		//Minimal - push, pull, and size
+	inline bool push(const T& V) { return size() <= S ? &(A[(R %= S)++] = V) : 0; }//adds to queue, returns true on success
 	inline T pull() { return R != F ? A[(F %= S)++] : T(); }//take from front of Queue, returns new initialized object on failure
-	inline T* head() { return size() ? &A[F] : 0; }//get pointer to beginning element, returns NULL pointer on failure
-	inline const T* head() const { return size() ? &A[F] : 0; }//Constant version of other head()
 	inline size_t size() const { return F > R ? S - F + R : R - F; }//get number of stored elements inside queue
+		//Head and rear reading - Not needed for functioning class
+	inline T* head() { return size() ? &A[F] : 0; }//get pointer to beginning element, returns NULL pointer on failure
+	inline const T* head() const { return head(); }//constant version of other head()
+	inline T* back() { return size() ? &A[(R + S - 1) % S] : 0; }//get pointer to last element, returns NULL pointer on failure
+	inline const T* back() const { return rear(); }//constant version of back()
+		//Capacity reading - Not needed for functioning class
+	inline size_t max() consteval { return S; }
 };
